1e6e9555926b1cafa0d6ce27268e4e8ac4f440e4
Devinpdl <samundra.pdl2000@gmail.com>
2025-05-21 10:21:35 +0545

    routing finished

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BookController@index')->name('books.index');
+Route::post('/books', 'BookController@store')->name('books.store');
+Route::get('/books/{book}', 'BookController@show')->name('books.show');
+Route::put('/books/{book}', 'BookController@update')->name('books.update');
+Route::delete('/books/{book}', 'BookController@destroy')->name('books.destroy');
+Route::get('/books/search/student', 'BookController@search')->name('books.search');
